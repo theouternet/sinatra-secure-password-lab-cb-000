61d7cb62922fa394c0cb1a28371a08b0178322e6
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-07-02 03:30:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,12 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+      if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    
   end
 
   get "/failure" do
